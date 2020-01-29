75bd6e5acf3a76c6a5c2dab204053a915ea8198e
Dhivya-car <59633655+Dhivya-car@users.noreply.github.com>
2020-01-29 23:25:56 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","admin","admin","shop") or die(mysqli_error($con));
-//$con=mysqli_connect("localhost","admin","","shop") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","admin","admin","store") or die(mysqli_error($con));
+//$con=mysqli_connect("localhost","admin","","store") or die(mysqli_error($con));
 ?>
